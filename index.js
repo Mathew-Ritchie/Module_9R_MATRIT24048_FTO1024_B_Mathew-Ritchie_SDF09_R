409e5237ade28fae956dfc75c8016aaab71739c0
Mathew Ritchie <ritchmat89@gmail.com>
2024-11-20 11:39:22 +0200

    getting syntax error on firstcard and after adding messageEl.textContent = message getting type errors

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
-
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
-}
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
 
-console.log(message);
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  // 2. Display the message in the messageEl using messageEl.textContent
+  messageEl.textContent = message;
+}
