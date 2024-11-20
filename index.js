5b51a3f2d1dae840f443cb7cd9c4d6646d35cb7e
Mathew Ritchie <ritchmat89@gmail.com>
2024-11-20 14:55:15 +0200

    working on new card button and array

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = 8;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
-// 1. Store the message-el paragraph in a variable called messageEl
 let messageEl = document.getElementById("message-el");
-console.log(messageEl);
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "sum: " + sum;
+
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "Wohoo! You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
-  // 2. Display the message in the messageEl using messageEl.textContent
+
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+
+  let card = 2;
+  sum += card;
+
+  renderGame();
 }
